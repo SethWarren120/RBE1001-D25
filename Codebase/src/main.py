0712174f42539af62b8374852f10cabd197c0fa1
sethwarren120 <150877648+SethWarren120@users.noreply.github.com>
2025-03-20 11:24:59 -0400

    Initial Commit

---
+++ Codebase/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       sethw                                                        #
+# 	Created:      3/20/2025, 11:24:29 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
