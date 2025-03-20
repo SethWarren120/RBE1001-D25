659a1d4ab4a45080dcaaf3975a421f9d9a6a764a
sethwarren120 <150877648+SethWarren120@users.noreply.github.com>
2025-03-20 12:43:27 -0400

    Added Lab-1

---
+++ Lab-1/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       sethw                                                        #
+# 	Created:      3/20/2025, 12:43:10 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
