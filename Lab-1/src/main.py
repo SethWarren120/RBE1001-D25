7df3da7e5f85ad1900e82250782d221d24db09bc
Z/Hacker_Man <78512843+haqer-man@users.noreply.github.com>
2025-03-20 15:33:10 -0400

    Update main.py

--- Lab-1/src/main.py
+++ Lab-1/src/main.py
@@ -14,19 +14,3 @@ from vex import *
 brain=Brain()
 
 brain.screen.print("Hello V5")
-
-gearRatio = 5
-# Brain should be defined by default
-left_motor = Motor(Ports.PORT1, 18_1, True)
-right_motor = Motor(Ports.PORT10, 18_1, False)
-arm_motor = Motor(Ports.PORT8, 18_1, True)
-
-brain.screen.print("Hello V5")
-
-left_motor.spin_for(FORWARD, 1 * gearRatio, TURNS, 30, RPM, False)
-right_motor.spin_for(FORWARD, 1 * gearRatio, TURNS, 30, RPM, True)
-
-wait(3000)
-# arm_motor.spin_for(REVERSE, .5, TURNS, 100, RPM)
-
-        
