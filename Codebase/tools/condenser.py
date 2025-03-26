e5b5dd8f194349018f788925bb6f35d1b7fde2d2
sethwarren120 <150877648+SethWarren120@users.noreply.github.com>
2025-03-25 23:02:08 -0400

    File Condenser Tool for code deployment
    Condenses all python files being used into one file for deployment to the robot. Open a new terminal and run the command "python tools/condenser.py" to update the deployment file. This must be done for new changes to be present in a code deployment

---
+++ Codebase/tools/condenser.py
@@ -0,0 +1,72 @@
+import json
+import os
+
+processed = []
+queue = [[]]
+files = {}
+filesDep = {}
+
+dependencies = [[]]
+
+dir = ""
+out = ""
+
+built = []
+imports = ""
+output = ""
+
+with open("tools/condenser_settings.json", "r") as file:
+    settings = json.loads(file.read())
+    dir = settings["projectPath"]
+    queue[0].append(settings["main"])
+    out = settings["output"]
+
+i = 0
+dependenceReached = False
+
+while not dependenceReached:
+    queue.append([])
+    dependencies.append([])
+    while len(queue[i]) > 0:
+        f = queue[i][0]
+        queue[i].remove(f)
+        dependencies[i].append(f)
+        if f in processed:
+            queue[i + 1] = queue[i + 1] + filesDep[f]
+            continue
+        processed.append(f)
+
+        with open(dir + f) as file:
+            
+            files[f] = ""
+            filesDep[f] = []
+            lines = file.read().splitlines()
+
+            for line in lines:
+                content = line.strip()
+                if content == "" or content.startswith("#"):
+                    continue
+                if content.startswith("from"):
+                    c2 = content.removeprefix("from").strip().split(' ', 1)[0].replace('.', '/') + ".py"
+                    if os.path.exists(dir + c2):
+                        queue[i+1].append(c2)
+                        filesDep[f].append(c2)
+                    elif not content in imports:
+                        imports = imports + content + "\n"
+                    continue
+                files[f] = files[f] + line + "\n"
+    i += 1
+    if len(queue[i]) == 0:
+        dependenceReached = True
+
+while i >= 0:
+    for dependence in dependencies[i]:
+        if dependence in built:
+            continue
+        
+        built.append(dependence)
+        output = output + files[dependence]
+    i -= 1
+
+with open(out, "w") as outputFile:
+    outputFile.write("# Automatically generated deployment script\n# Edits to this file will be overwritten\n" + imports + output)
